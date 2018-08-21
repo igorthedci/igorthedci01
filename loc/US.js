0e54136cc8a3a4d7dcb2c952fd060c653473473e
igorthedci <42576193+igorthedci@users.noreply.github.com>
2018-08-21 16:22:38 +0300

    test01
    test01

---
+++ loc/US.js
@@ -0,0 +1,15 @@
+//local texts:
+window.fontFamily = 'Arial';
+
+//buttons
+window.installBtnTitle = "Install"; //Install button title text
+window.installBtnTitleFont = "bold 34px " + fontFamily; //Install button title font style
+
+//tutorial hint
+window.hintMessageFirstLine = "Tap on the called out number";
+window.hintMessageSecondLine = "to mark it";
+window.hintFont = "bold 28px " + fontFamily;
+
+window.playBeforeText = "Play before installing"
+window.playBeforeFont = "italic 24px " + fontFamily;
+
